e96ed7b090857a2b6ec1220d1da034231707f467
Adam <adamff.me@gmail.com>
2023-11-03 18:12:18 +0100

    Add HTTP pages support

---
+++ background.js
@@ -0,0 +1,40 @@
+// background.js
+let clickedElText = '';
+
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    clickedElText = message.text;
+    console.log(clickedElText);
+});
+
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.contextMenus.create({
+        id: "copyElementText",
+        title: "Copy element inner text",
+        contexts: ["all"]
+    });
+});
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+    if (info.menuItemId === "copyElementText") {
+        copyTextToClipboard(clickedElText, tab);
+    }
+});
+
+function copyTextToClipboard(text, tab) {
+    console.log(text);
+    chrome.scripting.executeScript({
+        target: {tabId: tab.id},
+        function: functionToInject,
+        args: [text]
+    }, function() {
+        if (chrome.runtime.lastError) {
+            console.error(chrome.runtime.lastError.message);
+        } else {
+            console.log('Text copied to clipboard', text);
+        }
+    });
+}
+
+function functionToInject(text) {
+    simplecopy(text);
+}
